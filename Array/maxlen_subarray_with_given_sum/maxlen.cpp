367dfe68f2e618b2fbcff73e1d3568b5e40fef6f
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2021-06-29 23:29:20 +0530

    Create maxlen.cpp

---
+++ Array/maxlen_subarray_with_given_sum/maxlen.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int largestsubarray(int arr[], int n,int k)
+{
+  unordered_map<int, int> um;
+    int sum = 0, maxLen = 0;
+ 
+    // traverse the given array
+    for (int i = 0; i < n; i++) {
+ 
+        // accumulate sum
+        sum += arr[i];
+ 
+        // when subarray starts from index '0'
+        if (sum == k)
+            maxLen = i + 1;
+ 
+        // make an entry for 'sum' if it is
+        // not present in 'um'
+        if (um.find(sum) == um.end())
+            um[sum] = i;
+ 
+        // check if 'sum-k' is present in 'um'
+        // or not
+        if (um.find(sum - k) != um.end()) {
+ 
+            // update maxLength
+            if (maxLen < (i - um[sum - k]))
+                maxLen = i - um[sum - k];
+        }
+    }
+ 
+    // required maximum length
+    return maxLen;
+}
+
+int main ()
+{
+
+  int t;
+  cin >> t;
+
+  while (t--)
+    {
+      int n,k;
+      cin >> n>>k ;
+      int *arr = new int[n];
+
+      for (int i = 0; i < n; i++)
+	      cin >> arr[i];
+      cout<<largestsubarray(arr,n)<<endl;
+      
+    }
+  return 0;
+}
+     
