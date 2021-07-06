1e005cfa548be5872a2f4b661e45ab4d55b657f4
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2021-07-06 10:17:54 +0530

    Update rotate90.cpp

--- Matrix/rotate90.cpp
+++ Matrix/rotate90.cpp
@@ -1,4 +1,4 @@
-void rotate90(vector<int>&matrix){
+void rotate90(vector<vector<int>>&matrix){
   for(int i=0;i<matrix.size();i++){
    for(int j=i+1;j<matrix[0].size();j++){
     swap(matrix[i][j],matrix[j][i]); 
