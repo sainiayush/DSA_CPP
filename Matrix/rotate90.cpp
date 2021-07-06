bd719137105cc2bdfd0ca2361d313c3987078fb5
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2021-07-06 10:16:56 +0530

    Create rotate90.cpp

---
+++ Matrix/rotate90.cpp
@@ -0,0 +1,15 @@
+void rotate90(vector<int>&matrix){
+  for(int i=0;i<matrix.size();i++){
+   for(int j=i+1;j<matrix[0].size();j++){
+    swap(matrix[i][j],matrix[j][i]); 
+   }
+  }
+  for(int i=0;i<matrx[0].size();i++){
+  int lo=0, hi=matrix.size();
+  while(lo<hi){
+   swap(matrix[lo][i],matrix[hi][i]); 
+    lo++;
+    hi--;
+  }
+  }
+}
