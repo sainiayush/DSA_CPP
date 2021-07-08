ea1035a22105b6a0039ff5b479649a4e140d5d24
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2021-07-08 11:29:02 +0530

    Create merge_intervals.cpp

---
+++ Array/merge_intervals.cpp
@@ -0,0 +1,18 @@
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        
+        sort(intervals.begin(),intervals.end());
+        
+        vector<vector<int>>v;
+        v.push_back(intervals[0]);
+        
+        for(int i=1;i<intervals.size();i++){
+            if(v.back()[1]>=intervals[i][0])
+                v.back()[1]=max(v.back()[1],intervals[i][1]);
+            
+            else{
+                v.push_back(intervals[i]);
+            }
+        }
+        return v;
+    }
