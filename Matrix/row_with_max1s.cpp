bb4da13f28ed178af93cbde6ca23e493d963d5c6
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2021-07-08 23:09:04 +0530

    Create row_with_max1s.cpp

---
+++ Matrix/row_with_max1s.cpp
@@ -0,0 +1,18 @@
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    
+        
+      int i = 0;
+      int j = m-1;
+      int row = -1;
+      
+      while(i<n && j>=0){
+          if(arr[i][j] == 1 ){
+              row = i;
+              j--;
+          }
+          else{
+              i++;
+          }
+      }
+      return row;
+	}
