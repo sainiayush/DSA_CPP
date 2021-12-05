b45aeaff78edbee8ecc1348da90d4223a3b954af
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2021-12-05 22:18:31 +0530

    Create bstFromPreorder.cpp

---
+++ Trees/BST/bstFromPreorder.cpp
@@ -0,0 +1,12 @@
+TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i(0);
+        return build(preorder, i, INT_MAX);
+    }
+    TreeNode* build(vector<int>& A, int& i, int bound){
+        if(A.size()==i || A[i]>bound) return NULL;
+        
+        TreeNode* root = new TreeNode(A[i++]);
+        root->left = build(A, i, root->val);
+        root->right = build(A, i, bound);
+        return root;
+    }
