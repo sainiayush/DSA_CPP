626be532841e0399041886109f70149c29902f57
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2021-12-25 19:03:23 +0530

    Update leftView.cpp

--- Trees/Views/leftView.cpp
+++ Trees/Views/leftView.cpp
@@ -1,9 +1,8 @@
-void recursionLeft(Node *root, int level, vector<int> &res)
-    {
+void recursionLeft(Node *root, int level, vector<int> &res) {
         if(root==NULL) return ;
         if(res.size()==level) res.push_back(root->val);
-        recursionLeft(root->right, level+1, res);
         recursionLeft(root->left, level+1, res);
+        recursionLeft(root->right, level+1, res);
     }
 vector<int>leftView(Node *node){
   vector<int>ans;
