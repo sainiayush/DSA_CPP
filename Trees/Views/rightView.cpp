f2ba2c8f8a2eeb2c2346fbdc9f8a2cb85bcd7b72
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2021-12-25 19:04:16 +0530

    Update rightView.cpp

--- Trees/Views/rightView.cpp
+++ Trees/Views/rightView.cpp
@@ -1,3 +1,9 @@
+void recursionRight(Node *root, int level, vector<int> &res) {
+        if(root==NULL) return ;
+        if(res.size()==level) res.push_back(root->val);
+        recursionRight(root->right, level+1, res);
+        recursionRight(root->left, level+1, res);
+    }
 vector<int>rightView(Node *node){
   vector<int>ans;
 
