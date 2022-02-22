21776e2feb5c2476f01d90cd1307d905ec6acdc5
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2022-02-22 12:52:42 +0530

    Update BoundaryTraversal.cpp

--- Trees/BoundaryTraversal.cpp
+++ Trees/BoundaryTraversal.cpp
@@ -1,54 +1,74 @@
-bool isLeaf(Node *root){
-  return (!root->left && !root->right);
+/************************************************************
+
+    Following is the Binary Tree node structure:
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+bool isLeaf(TreeNode<int>* root){
+    return (root->left==NULL and root->right==NULL);
 }
-
-
-// Function for leftSide Boundary Traversal
-void leftTraversal(Node *root, vector<int>& ans){
-  Node* curr = root->left;
-  while(curr){
-    if(!isLeaf(curr)) ans.emplace_back(curr->data);
-    if(curr->left) curr = curr->left;
-    else curr = curr->right;
-  }
+void leftTraversal(vector<int>& ans,TreeNode<int>* root){
+    TreeNode<int>* node=root->left;
+    while(node){
+		if(!isLeaf(node)) ans.emplace_back(node->data);
+        if(node->left) node=node->left;
+        else node=node->right;
+    }
 }
-
-// Function for traversing all the leafs in inorder fashion
-void leafTraversal(Node *root, vector<int>& ans){
- 
-  if(isLeaf(root)) {
-    ans.emplace_back(root->data);
-    return;
-  }
-  if(root->left) leafTraversal(root->left,ans);
-  if(root->right) leafTraversal(root->right,ans);
+void leafTraversal(vector<int>&ans, TreeNode<int>* root){
+    TreeNode<int>* node=root;
+    if(isLeaf(node)){
+        ans.emplace_back(node->data);
+        return;
+    }
+    if(node->left) leafTraversal(ans,node->left);
+    if(node->right) leafTraversal(ans,node->right);
 }
-
-// Function for RightSide Boundary Traversal in reverse order
-void rightTraversal(Node *root, vector<int>& ans){
-  Node* curr= root->right;
-
-  stack<int>temp;
-
-  while(curr){
-   if(!isLeaf(curr)) temp.push(curr->data);
-    if(curr->right) curr = curr->right;
-    else curr = curr->left;
-  }
-
-  while(!temp.empty()){
-    ans.emplace_back(temp.top());
-    temp.pop();
-  }
+void rightTraversal(vector<int>&ans, TreeNode<int>* root){
+    TreeNode<int>* node=root->right;
+    
+    stack<TreeNode<int>*>stk;
+    
+    while(node){
+        if(!isLeaf(node)) stk.push(node);
+        if(node->right) node=node->right;
+        else node = node->left;
+    }
+    
+    while(!stk.empty()){
+        ans.emplace_back(stk.top()->data);
+        stk.pop();
+    }
 }
-vector<int> boundaryTraversal(Node *root){
-  vector<int>ans;
-  if(!root) return ans;
-
-  if(!isLeaf(root)) ans.emplace_back(root->data);
-  leftTraversal(root, ans);
-  leafTraversal(root, ans);
-  rightTraversal(root, ans);
-
-  return ans;
+vector<int> traverseBoundary(TreeNode<int>* root){
+    // Write your code here.
+    vector<int>ans;
+    if(!root) return ans;
+    
+    if(!isLeaf(root)) ans.emplace_back(root->data);
+    if(root->left) leftTraversal(ans,root);
+   	leafTraversal(ans,root);
+    if(root->right) rightTraversal(ans,root);
+    
+    return ans;
 }
