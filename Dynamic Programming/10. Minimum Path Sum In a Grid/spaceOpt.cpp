88a5511226809a1a4f0aad3688a3b64f3081a5a9
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2022-05-22 23:26:19 +0530

    Create spaceOpt.cpp

---
+++ Dynamic Programming/10. Minimum Path Sum In a Grid/spaceOpt.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minSumPath(int n, int m, vector<vector<int> > &matrix){
+      vector<int> prev(m,0);
+    for(int i=0; i<n ; i++){
+        vector<int> temp(m,0);
+        for(int j=0; j<m; j++){
+            if(i==0 && j==0) temp[j] = matrix[i][j];
+            else{
+                
+                int up = matrix[i][j];
+                if(i>0) up += prev[j];
+                else up += 1e9;
+                
+                int left = matrix[i][j];
+                if(j>0) left+=temp[j-1];
+                else left += 1e9;
+                
+                temp[j] = min(up,left);
+            }
+        }
+        prev=temp;
+    }
+    
+    return prev[m-1];
+    
+}
+
+
+
+int main() {
+
+  vector<vector<int> > matrix{{5,9,6},
+                            {11,5,2}};
+                            
+  int n = matrix.size();
+  int m = matrix[0].size();
+  
+  cout<<minSumPath(n,m,matrix);
+  
+  return 0;
+}
