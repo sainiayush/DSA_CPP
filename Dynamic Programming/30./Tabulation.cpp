526b659b56ee0209b6be6bcd70e65e164233c1d0
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2022-05-25 11:27:04 +0530

    Create Tabulation.cpp

---
+++ Dynamic Programming/30./Tabulation.cpp
@@ -0,0 +1,17 @@
+int findLCSLen(string& str,string& ptr,int n,int m){
+    vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(str[i-1]==ptr[j-1]) dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    return dp[n][m];
+}
+int canYouMake(string &str, string &ptr){
+    // Write your code here.
+    int n=str.size(), m=ptr.size();
+    int lcs=findLCSLen(str,ptr,n,m);
+    return (n+m-2*lcs);
+}
