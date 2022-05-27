ddf4eee77de899f371e9c202e162b8ede3439f37
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2022-05-27 23:15:43 +0530

    Create spaceOpt.cpp

---
+++ Dynamic Programming/39. Buy and Sell Stocks With Cooldown/spaceOpt.cpp
@@ -0,0 +1,13 @@
+int stockProfit(vector<int> &prices){
+    // Write your code here.
+    int n=prices.size();
+    vector<int> front1(2,0),front2(2,0),curr(2,0);
+    
+    for(int ind=n-1;ind>=0;ind--){
+            curr[1] = max(-prices[ind] + front1[0], front1[1]);
+            curr[0] = max(prices[ind] + front2[1], front1[0]);
+        front2=front1;
+        front1=curr;
+    }
+    return curr[1];
+}
