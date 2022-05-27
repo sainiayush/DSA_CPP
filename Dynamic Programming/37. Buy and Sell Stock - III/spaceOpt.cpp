69fee316f5ec1f1c4273798860dacdc7b7a51d4c
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2022-05-27 21:08:43 +0530

    Create spaceOpt.cpp

---
+++ Dynamic Programming/37. Buy and Sell Stock - III/spaceOpt.cpp
@@ -0,0 +1,20 @@
+int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> after(2, vector<int>(3,0)), curr(2, vector<int>(3,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    if(buy){
+                        curr[buy][cap] = max(-prices[ind] + after[0][cap], 
+                                                after[1][cap]);
+                    }else{
+                        curr[buy][cap] = max(prices[ind] + after[1][cap-1],
+                                               after[0][cap]);
+                    }
+                }
+            }
+            after=curr;
+        }
+        return after[1][2];
+    }
