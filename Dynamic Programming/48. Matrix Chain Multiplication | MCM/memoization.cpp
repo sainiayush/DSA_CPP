2885140714930e5a061ad954063916a1b385a2ff
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2022-05-30 22:05:07 +0530

    Create memoization.cpp

---
+++ Dynamic Programming/48. Matrix Chain Multiplication | MCM/memoization.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+int f(int i,int j,const vector<int>& arr,vector<vector<int>>& dp){
+    if(i==j) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int mini=1e9;
+    for(int k=i;k<j;k++){
+        int steps = arr[i-1]*arr[k]*arr[j] + f(i,k,arr,dp)+f(k+1,j,arr,dp);
+        mini=min(mini,steps);
+    }
+    return dp[i][j]=mini;
+}
+int matrixMultiplication(vector<int> &arr, int N){
+    // Write your code here.
+    vector<vector<int>> dp(N+1,vector<int>(N+1,-1));
+    return f(1,N-1,arr,dp);
+}
