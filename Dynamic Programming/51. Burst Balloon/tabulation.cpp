202911ee67827b1fa133fd5a430887c6d75908fc
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2022-05-31 12:37:55 +0530

    Create tabulation.cpp

---
+++ Dynamic Programming/51. Burst Balloon/tabulation.cpp
@@ -0,0 +1,20 @@
+#include<climits>
+int maxCoins(vector<int>& a){
+	// Write your code here.
+    int n=a.size();
+    a.insert(a.begin(),1);
+    a.emplace_back(1);
+    vector<vector<int>> dp(n+2, vector<int>(n+2,0));
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n;j++){
+            if(i>j) continue;
+            int maxi=INT_MIN;
+            for(int k=i;k<=j;k++){
+                int cost = a[i-1]*a[k]*a[j+1] + dp[i][k-1] + dp[k+1][j];
+                maxi=max(maxi,cost);
+            }
+            dp[i][j]= maxi;
+        }
+    }
+    return dp[1][n];
+}
