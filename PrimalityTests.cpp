198f95c82641b47ff98717addfbec8803d769b33
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2023-02-13 20:48:33 +0530

    Update PrimalityTests.cpp

--- PrimalityTests.cpp
+++ PrimalityTests.cpp
@@ -68,13 +68,30 @@ bool MillerRabin(int n,int iter=5){
   }
   return true;
 }
+bool fastMillerRabin(int n){
+  if(n < 2)
+    return false;
+  
+  int r=0;
+  int d=n-1;
+  while((d&1))
+    d >>= 1, ++r;
+
+  for(int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}){
+    if(n==a)
+      return true;
+    if(check_composite(n, a, d, r))
+      return false;
+  }
+  return true;
+}
 signed main(){
   int tt;
   cin >> tt;
   while(tt--){
     int N;
     cin >> N;
-    cout << (MillerRabin(N) ? "YES" : "NO");
+    cout << (fastMillerRabin(N) ? "YES" : "NO");
     if(tt) cout << endl;
   }
   return 0;
